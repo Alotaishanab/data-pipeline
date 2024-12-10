5c0ad41858e26322f138e4e5a7488c65579e7faa
abdullah alotaishan <abdullah.alotaishan@kcl.ac.uk>
2024-12-10 18:17:58 +0000

    clean terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,58 +2,48 @@ resource "random_id" "secret" {
   byte_length = 4
 }
 
-locals {
-  ssh_key      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDEGYMIHQdWQv/nwFf7zN2Pue60T+Outmr202vz3/DPAA+ufAWfo9FRw4r4dR44jFZpqmwIm2GCIH/JmB9/ETJsYgOJLBhxC+1Dea75jsQh7deYIcyfJQKJpnb0S5FJbg2+6H3jfGLj/MvoU/tVVqz2lNOmpApSj7B0Npo/qkea/3kzXhFz8Bhu4K1Glr0bh1d4YYLQ20SrSpFR+uGcwkKuOBMX12T25tNPADQ+Wi/nVK7jD9124oqcpgCGLDHWpSULX/AYCxCqMQOih3Kb2B6x7OPnCxXPpQugAOJ7zclfdRVpVN07RAPJKjMpVeQ+87EEH5ZFWpuOortUrrGE8xac+OsDOzQzmNCkchcq6rhs3YuPb/U86a6RswimkOGl/J2wT4Dd9npnCtkNyhFKc1Ucr+z63qQ/Pc0binfmgQ9XpX6A0FdMHs0d2XQzgKMjtDVfEEfclVO9ieGUMziDDdNzSce9xeAKWFyXZGXX3kBvmYDJOcs0HRjggUQXhmrHmbff6hkCd8qn2yoTIHtBgSH8VCR0tbT8UEyJFmqrBRt5qdDk4ITiPomdxOcEmdG9ivPtcDgcvP5RMNwUSBbEMWPGn7SHv36Glzm0t4Z4PPZouLr8pnrBzR9xUrd/5soyul0XkkcRGeA9Nj5ChtSsTMZbK26sk3UQjwNCm3+arAouSw== abdullah.alotaishan@kcl.ac.uk"
-  namespace    = "ucabbaa-comp0235-ns"
-  network_name = "ds4eng"
-  image_name   = "almalinux-9.4-20240805"
-  username     = "ucabbaa@ucl.ac.uk"
-}
-
 resource "harvester_cloudinit_secret" "cloud_config" {
-  name      = "${local.username}-cloudinit-${random_id.secret.hex}"
-  namespace = local.namespace
+  name      = "${var.username}-cloudinit-${random_id.secret.hex}"
+  namespace = var.provider_namespace
   user_data = <<-EOF
   #cloud-config
   ssh_authorized_keys:
-    - ${local.ssh_key}
+    - ${var.ssh_key}
   EOF
 }
 
 data "harvester_image" "img" {
-  name      = "image-bp52g"        # use the actual CR name from kubectl
-  namespace = "harvester-public"
+  name      = var.image_name
+  namespace = var.image_namespace
 }
 
-
-
-# Management VM: CPU=2, Mem=4Gi, Disk=10Gi
+# Management VM
 resource "harvester_virtualmachine" "mgmt" {
-  name                 = "${local.username}-mgmt-${random_id.secret.hex}"
-  namespace            = local.namespace
+  name                 = "${var.username}-mgmt-${random_id.secret.hex}"
+  namespace            = var.provider_namespace
   restart_after_update = true
 
-  description = "Management Node"
-  cpu    = 2
-  memory = "4Gi"
-  efi         = true
-  secure_boot = false
-  run_strategy    = "RerunOnFailure"
-  hostname        = "${local.username}-mgmt-${random_id.secret.hex}"
+  description   = "Management Node"
+  cpu           = var.mgmt_cpu
+  memory        = var.mgmt_memory
+  efi           = true
+  secure_boot   = false
+  run_strategy  = "RerunOnFailure"
+  hostname      = "${var.username}-mgmt-${random_id.secret.hex}"
   reserved_memory = "100Mi"
-  machine_type    = "q35"
+  machine_type  = "q35"
 
   network_interface {
     name           = "nic-1"
     wait_for_lease = true
     type           = "bridge"
-    network_name   = local.network_name
+    network_name   = var.network_name
   }
 
   disk {
     name       = "rootdisk"
     type       = "disk"
-    size       = "10Gi"
+    size       = var.mgmt_disk_size
     bus        = "virtio"
     boot_order = 1
     image      = data.harvester_image.img.id
@@ -65,34 +55,34 @@ resource "harvester_virtualmachine" "mgmt" {
   }
 }
 
-# Workers: 3 VMs, CPU=4, Mem=32Gi, Disk=25Gi
+# Worker VMs
 resource "harvester_virtualmachine" "worker" {
-  count               = 3
-  name                = "${local.username}-worker-${count.index + 1}-${random_id.secret.hex}"
-  namespace           = local.namespace
+  count               = var.worker_count
+  name                = "${var.username}-worker-${count.index + 1}-${random_id.secret.hex}"
+  namespace           = var.provider_namespace
   restart_after_update = true
 
-  description = "Worker Node"
-  cpu    = 4
-  memory = "32Gi"
-  efi         = true
-  secure_boot = false
-  run_strategy    = "RerunOnFailure"
-  hostname        = "${local.username}-worker-${count.index + 1}-${random_id.secret.hex}"
+  description   = "Worker Node"
+  cpu           = var.worker_cpu
+  memory        = var.worker_memory
+  efi           = true
+  secure_boot   = false
+  run_strategy  = "RerunOnFailure"
+  hostname      = "${var.username}-worker-${count.index + 1}-${random_id.secret.hex}"
   reserved_memory = "100Mi"
-  machine_type    = "q35"
+  machine_type  = "q35"
 
   network_interface {
     name           = "nic-1"
     wait_for_lease = true
     type           = "bridge"
-    network_name   = local.network_name
+    network_name   = var.network_name
   }
 
   disk {
     name       = "rootdisk"
     type       = "disk"
-    size       = "25Gi"
+    size       = var.worker_disk_size
     bus        = "virtio"
     boot_order = 1
     image      = data.harvester_image.img.id
@@ -104,33 +94,33 @@ resource "harvester_virtualmachine" "worker" {
   }
 }
 
-# Storage VM: CPU=4, Mem=8Gi, root=10Gi + extra 200Gi disk
+# Storage VM
 resource "harvester_virtualmachine" "storage" {
-  name                 = "${local.username}-storage-${random_id.secret.hex}"
-  namespace            = local.namespace
+  name                 = "${var.username}-storage-${random_id.secret.hex}"
+  namespace            = var.provider_namespace
   restart_after_update = true
 
-  description = "Storage Node"
-  cpu    = 4
-  memory = "8Gi"
-  efi         = true
-  secure_boot = false
-  run_strategy    = "RerunOnFailure"
-  hostname        = "${local.username}-storage-${random_id.secret.hex}"
+  description   = "Storage Node"
+  cpu           = var.storage_cpu
+  memory        = var.storage_memory
+  efi           = true
+  secure_boot   = false
+  run_strategy  = "RerunOnFailure"
+  hostname      = "${var.username}-storage-${random_id.secret.hex}"
   reserved_memory = "100Mi"
-  machine_type    = "q35"
+  machine_type  = "q35"
 
   network_interface {
     name           = "nic-1"
     wait_for_lease = true
     type           = "bridge"
-    network_name   = local.network_name
+    network_name   = var.network_name
   }
 
   disk {
     name       = "rootdisk"
     type       = "disk"
-    size       = "10Gi"
+    size       = var.storage_root_disk_size
     bus        = "virtio"
     boot_order = 1
     image      = data.harvester_image.img.id
@@ -140,7 +130,7 @@ resource "harvester_virtualmachine" "storage" {
   disk {
     name       = "datadisk"
     type       = "disk"
-    size       = "200Gi"
+    size       = var.storage_extra_disk_size
     bus        = "virtio"
     auto_delete = true
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "provider_endpoint" {
   type        = string
-  description = "Harvester API endpoint (e.g., https://<harvester_api_ip>:6443)"
+  description = "Harvester API endpoint"
 }
 
 variable "provider_token" {
@@ -11,15 +11,12 @@ variable "provider_token" {
 
 variable "provider_namespace" {
   type        = string
-  description = "Harvester namespace"
-  default     = "ucabbaa-comp0235-ns"
-  # If you have a custom namespace like ucabbaa-comp0235-ns and you have access, use that instead.
+  description = "Harvester namespace for VMs"
 }
 
 variable "username" {
   type        = string
   description = "Username prefix for naming VMs"
-  default     = "abdullah"
 }
 
 variable "ssh_key" {
@@ -29,47 +26,54 @@ variable "ssh_key" {
 
 variable "image_name" {
   type        = string
-  default     = "harvester-public/almalinux-9.4-20240805"
+  description = "Actual Harvester image resource name"
+}
+
+variable "image_namespace" {
+  type        = string
+  description = "Namespace where the image resides"
 }
 
 variable "network_name" {
   type        = string
-  default     = "ds4eng"
+  description = "Network name (may include namespace prefix)"
 }
 
 # Management VM specs
 variable "mgmt_cpu" {
   type    = number
-  default = 2
 }
-
 variable "mgmt_memory" {
   type    = string
-  default = "4Gi"
 }
-
 variable "mgmt_disk_size" {
   type    = string
-  default = "10Gi"
 }
 
 # Worker specs
 variable "worker_count" {
-  type    = number
-  default = 3
+  type = number
 }
-
 variable "worker_cpu" {
-  type    = number
-  default = 4
+  type = number
 }
-
 variable "worker_memory" {
-  type    = string
-  default = "32Gi"
+  type = string
+}
+variable "worker_disk_size" {
+  type = string
 }
 
-variable "worker_disk_size" {
-  type    = string
-  default = "25Gi"
+# Storage VM specs
+variable "storage_cpu" {
+  type = number
+}
+variable "storage_memory" {
+  type = string
+}
+variable "storage_root_disk_size" {
+  type = string
+}
+variable "storage_extra_disk_size" {
+  type = string
 }
